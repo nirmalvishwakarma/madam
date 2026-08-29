3e51a19e34e15765ec2654a7f6a2e095cf3f8d98
Nirmal <nirmalvishwakarma250@gmail.com>
2026-08-30 02:24:44 +0530

    Fix duplicate router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import EnterName from './component/EnterName';
-import Question from './component/Question';
-import LoveLetter from './component/LoveLetter';
-import Password from './component/Password';
-import Game from './component/Game';
-import Album from './component/Album';
+import { Route, Routes } from 'react-router-dom'
+
+import EnterName from './component/EnterName'
+import Question from './component/Question'
+import LoveLetter from './component/LoveLetter'
+import Password from './component/Password'
+import Game from './component/Game'
+import Album from './component/Album'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<EnterName />} />
-        <Route path='/password' element={<Password />} />
-        <Route path='/question' element={<Question />} />
-        <Route path='/game' element={<Game />} />
-        <Route path='/album' element={<Album />} />
-        <Route path='/loveLetter' element={<LoveLetter />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<EnterName />} />
+      <Route path="/password" element={<Password />} />
+      <Route path="/question" element={<Question />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/album" element={<Album />} />
+      <Route path="/loveLetter" element={<LoveLetter />} />
+    </Routes>
   )
 }
 
